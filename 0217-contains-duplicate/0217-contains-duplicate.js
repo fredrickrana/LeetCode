45922b27ac21410532e36a52683a93de39161dbd
Fredrick Rana <fredrickrana01@gmail.com>
2023-01-19 06:06:03 -0800

    Time: 93 ms (76.58%), Space: 59.5 MB (5.08%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -35,12 +35,49 @@
 // if value matches, return false
 // if no match, return true
 
+// var containsDuplicate = function(nums) {
+//     nums.sort()
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         if (nums[i] === nums[i + 1]) {
+//             return true;
+//         }
+//     }
+//     return false;
+// };
+
+// set / map
+// time: O(N)
+// space: O(N)
+
+// create a new set which will remove all duplicates within the array
+// use the size method to compare the element count to the array length
+// if values match, return false
+// if values do not match, return true
+
+// var containsDuplicate = function(nums) {
+//     const set = new Set(nums);
+//     return set.size !== nums.length;
+// };
+
+// create a map
+// check each element within the array
+// use the set method to store the current array element value by its key
+// at the beginning of each iteration, use the has method to check if the element value exists within the map
+// if it exists, return false
+// if there are no existing values, return true
+
 var containsDuplicate = function(nums) {
-    nums.sort()
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] === nums[i + 1]) {
-            return true;
+    let hashMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (hashMap.has(nums[i])) {
+           return true; 
+        } else {
+            hashMap.set(nums[i], i);
         }
     }
     return false;
 };
+
+
+
+
