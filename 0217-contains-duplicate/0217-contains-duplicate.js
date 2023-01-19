2ea8fe82e9ec2ccb3c699589c4ee6e26621ec5e2
Fredrick Rana <fredrickrana01@gmail.com>
2023-01-19 05:18:11 -0800

    Time: 177 ms (29.37%), Space: 51.1 MB (33.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -8,17 +8,38 @@
 // space: O(1)
 // no additional data structures used
 
-// check the value of the first element within the array
-// compare value to each following element within the array
+// check the value of each element within the array
+// compare value to each following elements within the array
+// if value matches, return false
+// if no match, return true
+
+// var containsDuplicate = function(nums) {
+//     for (let i = 0; i < nums.length; i++) { // time: O(N)
+//         for (let j = i + 1; j < nums.length; j++) { // time: O(N)
+//             if (nums[i] === nums[j]) {
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// };
+
+
+// sorting
+// time: O(N * log(N))
+// space: O(1)
+
+// use sort() method to return a sorted array with element values in ascending order
+// check the value of each element within the array
+// compare the value of the current element with the next element in the array
 // if value matches, return false
 // if no match, return true
 
 var containsDuplicate = function(nums) {
-    for (let i = 0; i < nums.length; i++) { // time: O(N)
-        for (let j = i + 1; j < nums.length; j++) { // time: O(N)
-            if (nums[i] === nums[j]) {
-                return true;
-            }
+    nums.sort()
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            return true;
         }
     }
     return false;
