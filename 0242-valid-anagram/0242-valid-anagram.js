3ff2f41af8e5a74995b3735c17d8740871720fa2
Fredrick Rana <fredrickrana01@gmail.com>
2023-01-20 22:13:08 -0800

    Time: 82 ms (85.71%), Space: 43.2 MB (75.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -21,3 +21,39 @@ var isAnagram = function(s, t) {
         return sortedS === sortedT;
     }
 };
+
+// hashmap
+
+// check to see if length of stringS is equal to length of stringT
+// if length does not match, return false
+// create two object literals for data storage
+// check the each character in the string and store within the object with a counter as its value
+// if the character already exists within the string, increment its counter
+// go through each key within each object and if the values do not match, return false, otherwise, return true
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    } else {
+        let mapS = {};
+        let mapT = {};
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] in mapS) {
+                mapS[s[i]]++;
+            } else {
+                mapS[s[i]] = 1;
+            }
+            if (t[i] in mapT) {
+                mapT[t[i]]++;
+            } else {
+                mapT[t[i]] = 1;
+            }
+        }
+        for (let key in mapS) {
+            if (mapT[key] !== mapS[key]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
