cddfd908481eccb0671322775c73ebcc14ef2adb
Fredrick Rana <fredrickrana01@gmail.com>
2023-01-25 13:09:54 -0800

    Time: 310 ms (26.22%), Space: 51.1 MB (53.04%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+    let palindrome = '';
+    let number = x.toString();
+    for (let i = number.length - 1; i >= 0; i--) {
+        palindrome += number[i];
+    }
+    return (parseInt(palindrome) === x);
+};
