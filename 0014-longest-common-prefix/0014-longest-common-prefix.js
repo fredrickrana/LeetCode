20dbdc80a1b8cc1c3ede2e1130b672afb1382a1b
Fredrick Rana <fredrickrana01@gmail.com>
2023-02-09 21:35:02 -0800

    Time: 79 ms (36.44%), Space: 42.8 MB (39.69%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+
+// check each word in the array
+// bracket notation 
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return '';
+    for (let i = 0; i <= strs[0].length; i++) {
+        if (!strs.every((string) => string[i] === strs[0][i])) {
+            return strs[0].slice(0, i);
+        }
+    }
+    return strs[0];
+};
