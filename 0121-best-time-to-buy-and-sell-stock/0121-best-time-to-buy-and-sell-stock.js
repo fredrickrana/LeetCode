f459571ed8f29ffa1fcaa860dca60bd89dcff91e
Fredrick Rana <fredrickrana01@gmail.com>
2023-06-16 18:18:42 -0700

    Time: 85 ms (54.56%), Space: 51.9 MB (19.56%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -25,23 +25,23 @@
 
 
 
-var maxProfit = function(prices) {
-    let left = 0;
-    let right = 1;
-    let max = 0;
+// var maxProfit = function(prices) {
+//     let left = 0;
+//     let right = 1;
+//     let max = 0;
     
-    while (right < prices.length) {
-        if (prices[right] <= prices[left]) {
-            left = right;
-        }
+//     while (right < prices.length) {
+//         if (prices[right] <= prices[left]) {
+//             left = right;
+//         }
         
-        const window = prices[right] - prices[left];
+//         const window = prices[right] - prices[left];
         
-        max = Math.max(max, window);
-        right++;
-    }
-    return max; 
-}
+//         max = Math.max(max, window);
+//         right++;
+//     }
+//     return max; 
+// }
 
 
 
@@ -49,17 +49,17 @@ var maxProfit = function(prices) {
 // APPROACH: BUY LOW, SELL HIGH
 // APPROACH: by keeping track of the minium price and calculating the potential profit at each step, it finds the maximum possible profit from buying and selling the stock
 
-// var maxProfit = function(prices) {
-//     let minPrice = Infinity; // set to Infinity to ensure that it will be updated with the lowest price encountered
-//     let maxProfit = 0; // set to zero, meaning no profit has been made yet
+var maxProfit = function(prices) {
+    let minPrice = Infinity; // set to Infinity to ensure that it will be updated with the lowest price encountered
+    let maxProfit = 0; // set to zero, meaning no profit has been made yet
     
-//     for (let i = 0; i < prices.length; i++) { // loop through the prices array, starting from first element (i = 0) and continues until the end (i < prices.length)
-//         if (prices[i] < minPrice) { // check to see if the current price is less than the current 'minPrice'
-//             minPrice = prices[i]; // if less than 'minPrice', 'minPrice' is updated to the current price. ensures 'minPrice' always holds the lowest price encountered so far
-//         } else if (prices[i] - minPrice > maxProfit) { // if current price is not less than 'minPrice', there is potential selling opportunity. Calc profit by subtracting 'minPrice' from current price
-//             maxProfit = prices[i] - minPrice; // if profit is greater than current 'maxProfit', update 'maxProfit'
-//         }
-//     }
-//     return maxProfit; // return maxProfit
-// }
+    for (let i = 0; i < prices.length; i++) { // loop through the prices array, starting from first element (i = 0) and continues until the end (i < prices.length)
+        if (prices[i] < minPrice) { // check to see if the current price is less than the current 'minPrice'
+            minPrice = prices[i]; // if less than 'minPrice', 'minPrice' is updated to the current price. ensures 'minPrice' always holds the lowest price encountered so far
+        } else if (prices[i] - minPrice > maxProfit) { // if current price is not less than 'minPrice', there is potential selling opportunity. Calc profit by subtracting 'minPrice' from current price
+            maxProfit = prices[i] - minPrice; // if profit is greater than current 'maxProfit', update 'maxProfit'
+        }
+    }
+    return maxProfit; // return maxProfit
+}
 
