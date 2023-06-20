ef4c21e581fbfd86a6508a478db2a2fa9c15b1fb
Fredrick Rana <fredrickrana01@gmail.com>
2023-06-19 19:42:10 -0700

    Time: 53 ms (95.91%), Space: 44.5 MB (35.86%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -15,34 +15,39 @@
 // TIME COMPLEXITY: O(N)
 // SPACE COMPLEXITY: O(1)
 
-var reverseList = function(head) {
-    if (head === null || head.next === null) {
-        return head;
-    }
-
-    let current = head;
-    let prev = null;    // pointer to track the next node
-    
-    while (current !== null) {
-        let next = current.next;    // pointer to track the next node
-        current.next = prev;    // reverse the pointer of the current node to point to the previous node
-        prev = current;     // move the prev pointer to the current node
-        current = next;     // move the current pointer to the next ndoe
-    }
-    
-    return prev;    // return the head of the reversed linked list
-}
-
-
-// APPROACH: RECURSION
-
 // var reverseList = function(head) {
 //     if (head === null || head.next === null) {
 //         return head;
 //     }
+
+//     let current = head;
+//     let prev = null;    // pointer to track the next node
     
-//     let reversedHead = reverseList(head.next);
-//     head.next.next = head;
-//     head.next = null;
-//     return reversedHead;
-// };
+//     while (current !== null) {
+//         let next = current.next;    // pointer to track the next node
+//         current.next = prev;    // reverse the pointer of the current node to point to the previous node
+//         prev = current;     // move the prev pointer to the current node
+//         current = next;     // move the current pointer to the next ndoe
+//     }
+    
+//     return prev;    // return the head of the reversed linked list
+// }
+
+
+// APPROACH: RECURSION
+// TIME COMPLEXITY: O(N)
+// SPACE COMPLEXITY: O(N)
+
+var reverseList = function(head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+    
+    let reversedHead = reverseList(head.next);
+    
+    head.next.next = head;
+    head.next = null;
+    return reversedHead;
+};
+
+// CONTINUE TO WORK ON THIS / UNDERSTAND THIS
