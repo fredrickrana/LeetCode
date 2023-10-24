ead13c62256f71c1502d916c61bf97cb270123cb
Fredrick Rana <fredrickrana01@gmail.com>
2023-10-24 03:04:35 -0700

    Time: 68 ms (74.80%), Space: 51.3 MB (85.48%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -63,3 +63,15 @@ var maxProfit = function(prices) {
     return maxProfit; // return maxProfit
 }
 
+var maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+    return maxProfit;
+}
