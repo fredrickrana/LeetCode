c080b3c069091228f9de9cead7e92ddd42b70d3b
Fredrick Rana <fredrickrana01@gmail.com>
2023-10-24 02:55:42 -0700

    Time: 46 ms (97.28%), Space: 42.7 MB (47.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -23,11 +23,25 @@
 
 
 // approach 2) hashmap
+// Time: O(n)
+// Memory: O(n)
 // create an object literal for data storage
 // look through each index within the array
 // subtract target by the selected index number and store within variable
 // if the stored value exists within the object, return the value and the index of the matching number
 // if the stored value does not exist, use bracket notation to store the value of the element in the array as a key with its index as its value
+
+// var twoSum = function(nums, target) {
+//     let map = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         const number = target - nums[i];
+//         if (number in map) {
+//             return [map[number], i]
+//         }
+//         map[nums[i]] = i;
+//     }
+// };
+
 
 var twoSum = function(nums, target) {
     let map = {};
@@ -40,5 +54,7 @@ var twoSum = function(nums, target) {
     }
 };
 
-
-
+// { 
+// 2:0
+// }
+// [0, 1]
