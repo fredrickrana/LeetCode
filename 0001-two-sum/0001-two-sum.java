87480a6b47c2eca3fd97905401a29eb83d72d63f
Fredrick Rana <fredrickrana01@gmail.com>
2025-10-10 08:25:56 -0700

    Time: 2 ms (98.96%), Space: 45.1 MB (48.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -12,15 +12,49 @@
 // if yes, return [i, j]
 // if no pairs match, return an empty array
 
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[]{i, j};
+//                 }
+//             }
+//         }
+//         return new int[]{};
+//     }
+// }
+
+// HashMap One-Pass
+// Time Complexity: O(n)
+// -> each number is only processed once
+// Space Complexity: O(n)
+// -> HashMap lookups (containsKey, get, put) are O(1) on average
+
+// Create an empty map to keep track of numebrs already seen
+// key will be number itself
+// value will be the index where we saw it
+
+// loop through array using index i
+// check if the complement is already in the map
+// if it is, number has been seen before and return with current number
+// return empty array if not found
+
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // stores value -> index
+
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            int complement = target - nums[i]; // what number we need to reach the target
+
+            if (map.containsKey(complement)) { // check if we already saw the complement 
+                return new int[] { map.get(complement), i }; // found the pair
             }
+
+            map.put(nums[i], i); // store current number with its index
         }
-        return new int[]{};
+
+        return new int[] {}; // no pair found (edge case)
     }
 }
