df77ece96653c81ccda4aa563522c47c778ace9a
Fredrick Rana <fredrickrana01@gmail.com>
2025-11-20 21:55:17 -0800

    Time: 2 ms (99.16%), Space: 47 MB (13.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -58,3 +58,22 @@ class Solution {
         return new int[] {}; // no pair found (edge case)
     }
 }
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer, Integer> map = new Hashmap<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             int number = target - nums[i];
+
+//             if (map.containsKey(number)) {
+//                 return new int[] { map.get(complement), i };
+//             }
+
+//             map.put(nums[i], i);
+//         }
+
+//         return new int[] {};
+//     }
+// }
